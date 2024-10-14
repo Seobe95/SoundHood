121a25f84e5f5458529b52dd51fe9aef0509b0f0
Seobe <woodyms@nate.com>
2024-10-14 15:28:00 +0900

    [Feat] Post단 설정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'seobe',
+      password: 'postgres',
+      database: 'soundhood-server',
+      entities: [__dirname + '/**/*.entity.{js, ts}'],
+      synchronize: true,
+      // 개발용에서만 true로 설정하기
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
