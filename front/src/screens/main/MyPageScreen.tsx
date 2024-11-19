7282a90d3fc3ccbfdf30c615473b1ca28a6d8ba9
Seobe <woodyms@nate.com>
2024-11-20 01:09:59 +0900

    [Style] 코드 수정 및 자잘한 코드 수정 - TabbarNavigator의 right header 삭제 - 사용하지 않는 import문 정리

--- front/src/screens/main/MyPageScreen.tsx
+++ front/src/screens/main/MyPageScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import CustomButton from '@/components/common/CustomButton.tsx';
+import useAuth from '@/hooks/queries/useAuth.ts';
+import { logout } from '@/api';
 
 interface MyPageScreenProps {}
 
 function MyPageScreen({}: MyPageScreenProps) {
-  return <View></View>;
+  const { logoutMutation } = useAuth();
+  return (
+    <View>
+      <CustomButton
+        label={'로그아웃'}
+        onPress={() => {
+          logoutMutation.mutate({});
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
