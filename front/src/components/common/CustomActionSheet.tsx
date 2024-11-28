21637b4c078087951305e6c63263e58e8b8eed5a
Seobe <woodyms@nate.com>
2024-11-28 17:57:02 +0900

    [Feat] CustomActionSheet 작업

---
+++ front/src/components/common/CustomActionSheet.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode, useContext } from 'react';
+import { View, StyleSheet, Modal, Pressable, Platform } from 'react-native';
+import { ThemeContext } from '@/context/CustomThemeContext.tsx';
+import { ColorsType } from '@/constants';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import useCustomActionSheetStore from '@/stores/useCustomActionSheetStore.ts';
+
+type CustomActionSheetProps = {
+  children: ReactNode;
+};
+
+function CustomActionSheet({ children }: CustomActionSheetProps) {
+  const { bottom } = useSafeAreaInsets();
+  const { hide, isOpen } = useCustomActionSheetStore();
+  const theme = useContext(ThemeContext);
+  const styles = makeStyle(theme);
+  const isAndroid = Platform.OS === 'android';
+
+  return (
+    <Modal animationType={'slide'} transparent={true} visible={isOpen}>
+      <Pressable onPress={hide} style={styles.transportContainer} />
+      <View
+        style={[
+          styles.bottomSheetContainer,
+          { height: 'auto', paddingBottom: isAndroid ? bottom + 16 : bottom },
+        ]}>
+        {children}
+      </View>
+    </Modal>
+  );
+}
+
+const makeStyle = (color: ColorsType) =>
+  StyleSheet.create({
+    transportContainer: {
+      height: '100%',
+    },
+    bottomSheetContainer: {
+      backgroundColor: color.backgroundColorSecondary,
+      position: 'absolute',
+      width: '100%',
+      overflow: 'hidden',
+      bottom: 0,
+      gap: 16,
+      padding: 16,
+      borderTopRightRadius: 16,
+      borderTopLeftRadius: 16,
+    },
+    font: {
+      color: color.fontColorPrimary,
+      fontSize: 68,
+    },
+  });
+
+export default CustomActionSheet;
