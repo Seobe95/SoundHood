18395f0ab8f57b9e17787b7311bb5a5993a92da3
Seobe <woodyms@nate.com>
2025-01-23 17:33:55 +0900

    [Fix] 기타 수정 - MapScreen 사용하지 않는 Import 제거 - 폰트 색상 미적용 수정 - APP_ID 환경변수 추가

--- front/src/types/react-native-config.d.ts
+++ front/src/types/react-native-config.d.ts
@@ -6,6 +6,8 @@ declare module 'react-native-config' {
     SPOTIFY_CLIENT_ID?: string;
     SPOTIFY_CLIENT_SECRET?: string;
     KAKAO_REST_API_KEY?: string;
+    KAKAO_NATIVE_APP_KEY?: string;
+    APP_ID?: string;
   }
 
   export const Config: NativeConfig;
