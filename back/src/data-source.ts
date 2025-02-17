bfa7e1e2d3647b629bd9c2e6651dd57636a25e00
Seobe <woodyms@nate.com>
2025-02-18 02:03:59 +0900

    [Setting] 설정

--- back/src/data-source.ts
+++ back/src/data-source.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
+import * as path from 'path';
 import { DataSource } from 'typeorm';
+
+const __dirname = path.resolve();
 
 export default new DataSource({
   type: 'postgres',
@@ -8,7 +11,7 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [__dirname + '/**/*.entity.{js, ts}'],
+  entities: [path.join(__dirname, '/**/*.entity.{js, ts}')],
   synchronize: false,
   // 개발용에서만 true로 설정하기
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
